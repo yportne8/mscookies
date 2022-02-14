1e1c98c335696ff1d6aa998e30f029283520758d
CS Kim <yportne8@gmail.com>
2022-02-14 16:23:25 -0500

    oops

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ URL = "https://github.com/yportne8/MS-Cookies/"
 EMAIL = 'yportne8@gmail.com'
 AUTHOR = 'CS Kim'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 REQUIRED = ["pycryptodome"]
 EXTRAS = {}
